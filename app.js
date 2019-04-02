0d9185a15e5bde2bf6071835e4446e112f885d12
leemww <48939966+leemww@users.noreply.github.com>
2019-04-03 00:30:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen((port) => {
+server.listen((port,'0.0.0.0') => {
   console.log(`Server running!!`);
 });
 
